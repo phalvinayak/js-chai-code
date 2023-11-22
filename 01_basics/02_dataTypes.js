f8354ae91613ba83d9bf362dfaa4bfa3bb854bf6
Vinayak Phal <phalvinayak@gmail.com>
2023-11-22 09:28:39 +0000

    Data Types completed.

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,23 @@
+"use strict"; // Treat all JS code as newer version
+
+// alert(2+3); // We're using nodejs, not browser.
+
+let age = 18;
+let name = 'Vinayak';
+let isLoggedIn = false;
+let state = null;
+
+// Premitive Data types
+// number => 2^53
+// bigint
+// string ''
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// Symbol => unique
+
+// Reference Data types
+// object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
--- README.md
+++ README.md
@@ -3,3 +3,7 @@ Chai aur Code JS series practice.
 
 ## Youtube playlist link
 - https://www.youtube.com/playlist?list=PLu71SKxNbfoBuX3f4EOACle2y-tRC5Q37
+
+## Javascript documentation sites
+- [tc39](https://tc39.es/)
+- [mdn developers](https://developer.mozilla.org/en-US/docs/Web/JavaScript)
